555285a2a2bb2260024dd85f72257e61653c6809
Yixin <chespinethan@gmail.com>
2026-02-03 10:42:09 -0500

    feature: css styling

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,9 +1,10 @@
+import "../css/Favorites.css"
 
-function Favourite() {
-    return <div className="favourite-empty">
+function Favourites() {
+    return <div className="favorites-empty">
         <h2>No Favourite Movies Yet</h2>
         <p>Start adding movies to your favourites and they will appear here</p>
     </div>
 }
 
-export default Favourite
+export default Favourites
